52aa0d9828b4c289ff6be96e60d7622975acb655
noor-46labs <93204082+noor-46labs@users.noreply.github.com>
2021-10-27 13:14:44 +0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   has_many :user_role
   has_many :books
   has_many :reviews
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
